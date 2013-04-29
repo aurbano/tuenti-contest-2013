e8aefbcb119a192ea881af7a8258c7cfb15184fb
alejandro@urbanoalvarez.es <alejandro@urbanoalvarez.es>
2013-04-29 15:46:44 +0200

    P2 halfway

---
+++ p2/src/es/urbanoalvarez/P2.java
@@ -0,0 +1,51 @@
+package es.urbanoalvarez;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P2 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in)); 
+		String s;
+		int line = -1,
+			suggestions = 0;
+		String dict = "";
+		Search[] searchers = new Search[0];
+		
+		while ((s = in.readLine()) != null && s.length() != 0){	
+			 line++;
+			 //System.out.println("Reading line "+line);
+			 if(line == 0) continue;
+			 if(line == 1){
+				 dict = s;
+				 //System.out.println("Using dict="+dict);
+				 continue;
+			 }
+			 if(line == 3){
+				 suggestions = new Integer(s);
+				 searchers = new Search[suggestions];
+				 //System.out.println("Suggest="+suggestions);
+				 continue;
+			 }
+			 if(line < 5){
+				 //System.out.println("line<5 cont");
+				 continue;
+			 }
+			 //System.out.println("launching "+(line-5));
+			 searchers[line-5] = new Search(dict, s); // Launch the searchers
+			 for(int i=0; i < suggestions; i++){
+				 try{
+					 searchers[i].join();
+					 System.out.print(searchers[i].w+" -> ");
+					 for(int a=0; a<searchers[i].found.size(); a++){
+						 System.out.print(searchers[i].found.get(a)+" ");
+					 }
+					 System.out.println();
+				 }catch(Exception e){
+					 e.printStackTrace();
+				 }
+			 }
+		}
+	}
+}
---
+++ p2/src/es/urbanoalvarez/Search.java
@@ -0,0 +1,48 @@
+package es.urbanoalvarez;
+
+import java.io.*;
+import java.util.*;
+
+public class Search extends Thread{
+	String dict, w;
+	char[] word;
+	
+	ArrayList<String> found = new ArrayList<String>();
+	
+	public Search(String d, String w){
+		//System.out.println("Search: dict="+d+"; word="+w);
+		this.w = w;
+		dict = d;
+		word = w.toCharArray();
+		Arrays.sort(word);
+		this.start();
+	}
+	
+	public void run(){
+		// Open dictionary
+		try{
+			BufferedReader br = new BufferedReader(new FileReader(new File (dict)));
+			String line;
+			
+			// Create char array for searched word
+			
+			while((line = br.readLine()) != null){
+				if(sameChars(line)){
+					//System.out.println("Found: "+line);
+					found.add(line);
+				}
+			}
+			
+			br.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	// Compare two strings' characters
+	private boolean sameChars(String check) {
+	  char[] first = check.toCharArray();
+	  Arrays.sort(first);
+	  return Arrays.equals(first, word);
+	}
+}
