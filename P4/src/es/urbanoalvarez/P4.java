9370b1babf5a2bdeb08b8254cd8d425ad0c47df5
Alejandro U. Alvarez <alejandro@urbanoalvarez.es>
2013-04-29 18:52:29 +0200

    Exclude binaries

--- P4/src/es/urbanoalvarez/P4.java
+++ P4/src/es/urbanoalvarez/P4.java
@@ -1,5 +1,15 @@
 package es.urbanoalvarez;
 
-public class P4 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class P4 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in)); 
+		String s;
+		while ((s = in.readLine()) != null && s.length() != 0){
+			
+		}
+	}
 }
