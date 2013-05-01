06e1ea7a913dcd976d4bb88da4dd25fda87c1746
Alejandro U. Alvarez <alejandro@urbanoalvarez.es>
2013-05-01 14:19:46 +0200

    Initial P7 files

---
+++ p7/src/es/urbanoalvarez/P7.java
@@ -0,0 +1,12 @@
+package es.urbanoalvarez;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P7{
+	public static void main(String[] args) throws IOException{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
